ac725a638d7c22be63ff48c35b1e7cf489fe1c7c
Josiah Koehler <josiah3925@gmail.com>
2021-03-05 06:18:29 -0500

    Create course.php

---
+++ course.php
@@ -0,0 +1,12 @@
+<div class="header">
+  <div class="head1">
+    <h2>IT 207 DL7 Sprint 2021</h2>
+    <p>Instructor<br/>George Mason University</p>
+  </div>
+  <div class="head2">
+    <h2>Josiah Koehler</h2>
+    <a href="mailto:jkoehle@masonlive.gmu.edu"></a>
+    <?php "<p>Last Date Modified:", date("F d Y H:i:s", getlastmod()), "EST <br/></p>";
+    ?>
+  </div>
+</div>
