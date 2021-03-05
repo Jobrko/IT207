8b5c242281dd36501c75d662a970bb7223df3a8d
Josiah Koehler <josiah3925@gmail.com>
2021-03-05 06:23:58 -0500

    update

---
+++ nav.php
@@ -0,0 +1,15 @@
+<div class="assignments">
+  <h3><b>Assignments</b></h3>
+  <ul class="a">
+    <li>Link1</li>
+    <li>Link2</li>
+    <li>Link3</li>
+    <li>Link4</li>
+  </ul>
+  <h3><b>Lab Practicum</b></h3>
+  <ul>
+    <li>Link1</li>
+    <li>Link2</li>
+    <li>Link Home</li>
+  </ul>
+</div>
